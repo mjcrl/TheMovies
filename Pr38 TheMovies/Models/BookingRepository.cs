01e08719ab9ae758ac80ded1d6989a29660aaecb
cvHanky <145098299+cvHanky@users.noreply.github.com>
2024-02-01 10:57:07 +0100

    Reverted back to Repositories having responsibility of load and save.

--- Pr38 TheMovies/Models/BookingRepository.cs
+++ Pr38 TheMovies/Models/BookingRepository.cs
@@ -20,7 +20,25 @@ namespace Pr38_TheMovies.Models
 
         private void initRepo()
         {
-            bookings.Add((Booking)DataHandler.initRepo("BOOKING"));
+            using StreamReader myReader = new StreamReader("Pr38_TheMoviesMedBilletAntal.csv");
+            {
+                string line = myReader.ReadLine();
+                Movie movie = null;
+                Showing showing = null;
+                Booking booking = null;
+                while (line != null)
+                {
+                    string[] parts = line.Split(';');
+                    movie = new Movie(parts[3], parts[4], parts[5], parts[6], DateTime.Parse(parts[7]) );
+                    showing = new Showing(DateTime.Parse(parts[2]), 50, movie);
+                    booking = new Booking(int.Parse(parts[10]), parts[8], parts[9], showing);
+                    if (booking != null )
+                    {
+                        bookings.Add(booking);
+                    }
+                    line = myReader.ReadLine();
+                }
+            }
         }
 
         public Booking AddBooking(int ticketAmount, string email, string phoneNumber, Showing showingTBS)
