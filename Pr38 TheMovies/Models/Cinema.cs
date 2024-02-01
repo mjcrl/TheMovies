cd3b3451b21030955fd92bceb1efe8185d1a5ae5
cvHanky <145098299+cvHanky@users.noreply.github.com>
2024-02-01 10:12:08 +0100

    Added Cinema class, updated DCD.

---
+++ Pr38 TheMovies/Models/Cinema.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr38_TheMovies.Models
+{
+    public class Cinema
+    {
+        public string City { get; set; }
+        public string Name { get; set; }
+
+        public Cinema(string city, string name)
+        {
+            City = city;
+            Name = name;
+        }
+    }
+}
---
+++ Pr38 TheMovies/Models/MovieRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr38_TheMovies.Models
+{
+    public class MovieRepository
+    {
+        private ObservableCollection<Movie> movies;
+
+        public MovieRepository()
+        {
+            movies = new ObservableCollection<Movie>();
+            // InitRepo();
+        }
+
+        private void InitRepo() { }
+
+        public Movie AddMovie(string title, string genre, string duration, string instructor, DateTime premiere)
+        {
+            Movie m = new Movie(title, genre, duration, instructor, premiere);
+            movies.Add(m);
+            return m;
+        }
+
+        public void DeleteMovie(Movie movie)
+        {
+            movies.Remove(movie);
+        }
+    }
+}
