d3957cce1c06b665741c6c16f40bdb15d8e0bf57
cvHanky <145098299+cvHanky@users.noreply.github.com>
2024-02-01 10:13:21 +0100

    Added Location property to Showing.

--- Pr38 TheMovies/Models/Showing.cs
+++ Pr38 TheMovies/Models/Showing.cs
@@ -12,15 +12,17 @@ namespace Pr38_TheMovies.Models
         public DateTime PlayingDate { get; set; }
         public int SeatCapacity { get; set; }
         public Movie MovieTBS { get; set; }
+        public Cinema Location { get; set; }
 
         #endregion
 
         #region Constructors
-        public Showing(DateTime playingDate,  int seatCapacity, Movie movieTBS)
+        public Showing(DateTime playingDate,  int seatCapacity, Movie movieTBS, Cinema location)
         {
             PlayingDate = playingDate;
             SeatCapacity = seatCapacity;
             MovieTBS = movieTBS;
+            Location = location;
         }
         #endregion
     }
