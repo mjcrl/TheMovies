1dce363cc8d5ff6d87b17b2de19eea853abf4648
cvHanky <145098299+cvHanky@users.noreply.github.com>
2024-02-01 09:16:05 +0100

    Updated DCD to include city and name for cinema. Added ShowingRepo.

---
+++ Pr38 TheMovies/Models/ShowingRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr38_TheMovies.Models
+{
+    public class ShowingRepository
+    {
+        private ObservableCollection<Showing> showings;
+
+        public ShowingRepository()
+        {
+            showings = new ObservableCollection<Showing>();
+            // InitRepo();
+        }
+
+        private void InitRepo() { }
+
+        public Showing AddShowing
+    }
+}
